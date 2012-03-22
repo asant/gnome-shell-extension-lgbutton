43c8edd65bd524587a0e4514c00f67723af0dd40
asant <andreasantilli gmx com>
2012-03-22 18:46:17 +0100

    more reliable way to detect the localization directory

--- src/extension.js
+++ src/extension.js
@@ -28,12 +28,14 @@ const Panel     = imports.ui.panel;
 const PanelMenu = imports.ui.panelMenu;
 const St        = imports.gi.St;
 
-const LOCALE_SUBDIR = 'locale';
-
-const TOOLTIP   = "Toggle LookingGlass";
-const ICON_NAME = 'preferences-desktop-display';
-const ICON_TYPE = St.IconType.SYMBOLIC;
-const ROLE      = 'lgbutton';
+const LOCALE_SUBDIR     = 'locale';
+const LOCALE_EXT        = '.mo';
+const MSG_SUBDIR        = 'LC_MESSAGES';
+const NEW_API_VERSION   = '3.3.90';
+const TOOLTIP           = "Toggle LookingGlass";
+const ICON_NAME         = 'preferences-desktop-display';
+const ICON_TYPE         = St.IconType.SYMBOLIC;
+const ROLE              = 'lgbutton';
 
 function LookingGlassButton() {
     this._init.apply(this, arguments);
@@ -91,24 +93,41 @@ let lgb;
 let Gettext;
 let _;
 
+/* emulating python's gettext.find() behavior */
 function init_localizations(metadata) {
-    let data_dirs = new Array(metadata.path);
-    data_dirs = data_dirs.concat(GLib.get_system_data_dirs());
+    let domain = metadata.uuid;
+    let langs = GLib.get_language_names();
+    let locale_dirs = new Array(GLib.build_filenamev([metadata.path,
+            LOCALE_SUBDIR]));
 
     /* I prefer to fetch the uuid from the metadata instead of hardcoding it */
     Gettext = imports.gettext.domain(metadata.uuid);
     _ = Gettext.gettext;
 
-    for (let i = 0; i < data_dirs.length; i++) {
-        let dir = Gio.file_new_for_path(GLib.build_filenamev([ data_dirs[i],
-                LOCALE_SUBDIR ]));
+    /* check whether we're using the right shell version before trying to fetch 
+     * its locale directory */
+    if (imports.misc.config.PACKAGE_VERSION < NEW_API_VERSION) {
+        let data_dirs = GLib.get_system_data_dirs();
+        for (let i = 0; i < data_dirs.length; i++) {
+            locale_dirs = locale_dirs.concat([ GLib.build_filenamev([
+                    data_dirs[i], LOCALE_SUBDIR ]) ]);
+        }
+    } else {
+        locale_dirs = locale_dirs.concat([ imports.misc.config.LOCALEDIR ]);
+    }
 
-        if ((dir.query_exists(null)) && 
-                (dir.query_file_type(Gio.FileQueryInfoFlags.NONE, null) ==
-                Gio.FileType.DIRECTORY)) {
-            imports.gettext.bindtextdomain(metadata.uuid, dir.get_path());
-            imports.gettext.textdomain(metadata.uuid);
-            break;
+    for (let j = 0; j < locale_dirs.length; j++) {
+        for (let i = 0; i < langs.length; i++) {
+            let str = GLib.build_filenamev([ locale_dirs[j], langs[i],
+                    MSG_SUBDIR, domain ]) + LOCALE_EXT;
+            let file = Gio.file_new_for_path(str);
+
+            if (file.query_file_type(Gio.FileQueryInfoFlags.NONE, null) ==
+                    Gio.FileType.REGULAR) {
+                imports.gettext.bindtextdomain(domain, locale_dirs[j]);
+                imports.gettext.textdomain(domain);
+                return;
+            }
         }
     }
 }
